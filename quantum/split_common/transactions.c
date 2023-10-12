67fd46d35e602b3d33b6eaf70be7252961586c9b
Giuseppe Rota <403432+grota@users.noreply.github.com>
2023-10-12 19:48:55 +0200

    haptic_handlers_slave does not trigger haptic_play()

--- quantum/split_common/transactions.c
+++ quantum/split_common/transactions.c
@@ -806,11 +806,6 @@ static bool haptic_handlers_master(matrix_row_t master_matrix[], matrix_row_t sl
 
 static void haptic_handlers_slave(matrix_row_t master_matrix[], matrix_row_t slave_matrix[]) {
     memcpy(&haptic_config, &split_shmem->haptic_sync.haptic_config, sizeof(haptic_config_t));
-
-    if (split_shmem->haptic_sync.haptic_play != 0xFF) {
-        haptic_set_mode(split_shmem->haptic_sync.haptic_play);
-        haptic_play();
-    }
 }
 
 // clang-format off
